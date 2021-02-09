6a32c18c4fea6f7ff7a169b8ca24c16b80608bca
Spyros <spirosgsaaa@gmail.com>
2021-02-09 18:40:55 +0200

    Update

--- src/main/java/org/spyrosmouchlianitis/todolist/App.java
+++ src/main/java/org/spyrosmouchlianitis/todolist/App.java
@@ -4,4 +4,11 @@ public class App {
     public static void main(String[] args) {
 
     }
+
+    private void menu() {
+        System.out.println("1. Add task");
+        System.out.println("2. Check task");
+        System.out.println("3. Remove Task");
+        System.out.println("4. Quit");
+    }
 }
